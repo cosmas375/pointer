9b5af636c94ac54abb323b060896cbaed8d37b8b
Roman Lyakhovchik <roman.lyakhovchik@gmail.com>
2021-01-08 21:39:45 +0300

    popup layout

---
+++ utils/localizeHTML.js
@@ -0,0 +1,20 @@
+(() => {
+    const components = document.querySelectorAll('[data-content]');
+
+    if (!components) {
+        return;
+    }
+
+    [...components].forEach(component => {
+        const translationKey = component.dataset.content;
+
+        if (!translationKey.trim) {
+            return;
+        }
+
+        const translation = chrome.i18n.getMessage(translationKey);
+        if (translation) {
+            component.innerText = translation;
+        }
+    });
+})();
