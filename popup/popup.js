f89fbdc1a2af11648adc0bd8a5702822100fa416
Roman Lyakhovchik <roman.lyakhovchik@gmail.com>
2021-01-26 00:54:00 +0300

    messaging from extension to content scripts

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,12 @@
 const container = document.querySelector('.container');
 
 container.addEventListener('click', e => {
-    if (!e.target?.dataset?.pointerType) {
+    const pointerType = e.target?.dataset?.pointerType;
+    if (!pointerType) {
         return;
     }
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { pointerType });
+    });
+    window.close();
 });
